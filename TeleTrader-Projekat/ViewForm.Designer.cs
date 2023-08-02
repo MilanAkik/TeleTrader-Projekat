47e41840b559d172801e7bd0f976c6f2601bff1c
MilanAkik <mixa.mixony@gmail.com>
2023-08-02 19:41:03 +0200

    Designed the first form

--- TeleTrader-Projekat/ViewForm.Designer.cs
+++ TeleTrader-Projekat/ViewForm.Designer.cs
@@ -29,25 +29,172 @@
         private void InitializeComponent()
         {
             menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            dataGridView1 = new DataGridView();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // menuStrip1
             // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, helpToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Size = new Size(800, 24);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
-            // Form1
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(103, 22);
+            openToolStripMenuItem.Text = "Open";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(107, 22);
+            aboutToolStripMenuItem.Text = "About";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 61);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(776, 356);
+            dataGridView1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(49, 31);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(179, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(315, 31);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(179, 23);
+            comboBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(516, 30);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Filter";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(34, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Type:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(251, 34);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Exchange:";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 423);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Add symbol";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(112, 423);
+            button3.Name = "button3";
+            button3.Size = new Size(116, 23);
+            button3.TabIndex = 8;
+            button3.Text = "View/Edit symbol";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(234, 423);
+            button4.Name = "button4";
+            button4.Size = new Size(97, 23);
+            button4.TabIndex = 9;
+            button4.Text = "Delete symbol";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(712, 423);
+            button5.Name = "button5";
+            button5.Size = new Size(76, 23);
+            button5.TabIndex = 10;
+            button5.Text = "Form close";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // ViewForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(dataGridView1);
             Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            KeyPreview = true;
             MainMenuStrip = menuStrip1;
-            Name = "Form1";
-            Text = "Form1";
+            Name = "ViewForm";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "View Form";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +202,19 @@
         #endregion
 
         private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private DataGridView dataGridView1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
